11beae801f41b4bfdfa1b6650dae459a585be363
Vincent Wong <65081301+KWLON@users.noreply.github.com>
2022-01-27 21:42:39 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function IndexPage() {
+  return (
+    <div>
+      ASDADASDDASd.{" "}
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+}
